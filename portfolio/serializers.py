509688ced5bab32aca56364a19d0f1df813c88a7
Arindam Shukla <arindamshrish@gmail.com>
2025-12-29 14:16:50 +0530

    Fix serializers to work with CharField URLs

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -2,52 +2,14 @@ from rest_framework import serializers
 from .models import Profile, Project, Skill, Experience, Achievement, Education, Contact
 
 class ProfileSerializer(serializers.ModelSerializer):
-    profile_image = serializers.SerializerMethodField()
-    about_image = serializers.SerializerMethodField()
-    resume_url = serializers.SerializerMethodField()
-    
     class Meta:
         model = Profile
         fields = '__all__'
-    
-    def get_profile_image(self, obj):
-        if obj.profile_image:
-            request = self.context.get('request')
-            if request:
-                return request.build_absolute_uri(obj.profile_image.url)
-            return obj.profile_image.url
-        return None
-    
-    def get_about_image(self, obj):
-        if obj.about_image:
-            request = self.context.get('request')
-            if request:
-                return request.build_absolute_uri(obj.about_image.url)
-            return obj.about_image.url
-        return None
-    
-    def get_resume_url(self, obj):
-        if obj.resume_url:
-            request = self.context.get('request')
-            if request:
-                return request.build_absolute_uri(obj.resume_url.url)
-            return obj.resume_url.url
-        return None
 
 class ProjectSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
-    
     class Meta:
         model = Project
         fields = '__all__'
-    
-    def get_image(self, obj):
-        if obj.image:
-            request = self.context.get('request')
-            if request:
-                return request.build_absolute_uri(obj.image.url)
-            return obj.image.url
-        return None
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
